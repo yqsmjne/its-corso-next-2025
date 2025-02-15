e97c2e68e65b8cc00c4817e961e63471a22ac008
Lorenzo Russo <lorenzo.2000.russo@gmail.com>
2025-02-15 18:02:01 +0100

    Refactor Today component to use Movie type and enhance card layout with additional buttons and image display

--- src/lib/Homepage/Today.tsx
+++ src/lib/Homepage/Today.tsx
@@ -1,72 +1,81 @@
 'use client';
 
 import { Carousel, CarouselSlide } from '@mantine/carousel';
-import { Button, Paper, Text, Title, useMantineTheme } from '@mantine/core';
+import { Button, Flex, Image, Paper, Stack, Text, Title, useMantineTheme } from '@mantine/core';
 import { useMediaQuery } from '@mantine/hooks';
 import classes from './Today.module.css';
 
 import '@mantine/carousel/styles.css';
 
-function Card({ image, title, category }) {
+import { Movie } from '@/api-types/movie';
+
+function Card({ movie }: { movie: Movie }) {
   return (
     <Paper
       shadow="md"
       p="xl"
       radius="md"
-      style={{ backgroundImage: `url(${image})` }}
+      style={{ backgroundImage: `url(${movie.image_url})`, filter: 'blur(10px)' }}
       className={classes.card}
     >
-      <div>
-        <Text className={classes.category} size="xs">
-          {category}
-        </Text>
-        <Title order={3} className={classes.title}>
-          {title}
-        </Title>
-      </div>
-      <Button variant="white" color="dark">
-        Read article
-      </Button>
+      <Flex align="center" justify="space-around" w="100%" h="100%" gap="xl">
+        <Stack gap="xs" align="start" h="100%">
+          <Text className={classes.category} size="xs">
+            {movie.genre}
+          </Text>
+          <Title order={3} className={classes.title}>
+            {movie.title}
+          </Title>
+
+          <Button variant="white" color="dark">
+            Acquista il biglietto
+          </Button>
+          <Button variant="white" color="dark">
+            Vai alla scheda
+          </Button>
+        </Stack>
+        <Image src={movie.image_url} h="100%" alt={movie.title} />
+      </Flex>
     </Paper>
   );
 }
 
-const data = [
+const data: Array<Movie> = [
   {
-    image:
-      'https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    title: 'Best forests to visit in North America',
-    category: 'nature',
+    id: 1,
+    title: 'The Shawshank Redemption',
+    genre: 'Drama',
+    language: 'English',
+    director: 'Frank Darabont',
+    cast: ['Tim Robbins', 'Morgan Freeman', 'Bob Gunton'],
+    description: 'Two imprisoned',
+    year: 1994,
+    image_url: '/film/5f3ba8be-4748-451e-a694-b8156e2b529c-locandina-e-la-festa-continua.jpg',
+    duration: 142,
   },
   {
-    image:
-      'https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    title: 'Hawaii beaches review: better than you think',
-    category: 'beach',
+    id: 2,
+    title: 'The Shawshank Redemption',
+    genre: 'Drama',
+    language: 'English',
+    director: 'Frank Darabont',
+    cast: ['Tim Robbins', 'Morgan Freeman', 'Bob Gunton'],
+    description: 'Two imprisoned',
+    year: 1994,
+    image_url: '/film/1131w-nduFAHrWNmU.webp',
+    duration: 142,
   },
   {
-    image:
-      'https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    title: 'Mountains at night: 12 best locations to enjoy the view',
-    category: 'nature',
-  },
-  {
-    image:
-      'https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    title: 'Aurora in Norway: when to visit for best experience',
-    category: 'nature',
-  },
-  {
-    image:
-      'https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    title: 'Best places to visit this winter',
-    category: 'tourism',
-  },
-  {
-    image:
-      'https://images.unsplash.com/photo-1582721478779-0ae163c05a60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-    title: 'Active volcanos reviews: travel at your own risk',
-    category: 'nature',
+    id: 3,
+    title: 'Brutalist',
+    genre: 'Drama',
+    language: 'English',
+    director: 'Frank Darabont',
+    cast: ['Tim Robbins', 'Morgan Freeman', 'Bob Gunton'],
+    description: 'Two imprisoned',
+    year: 1994,
+    image_url: '/film/65994.jpg',
+    duration: 142,
   },
 ];
 
@@ -74,8 +83,8 @@ export const Today = () => {
   const theme = useMantineTheme();
   const mobile = useMediaQuery(`(max-width: ${theme.breakpoints.sm})`);
   const slides = data.map((item) => (
-    <CarouselSlide key={item.title}>
-      <Card {...item} />
+    <CarouselSlide key={item.id}>
+      <Card movie={item} />
     </CarouselSlide>
   ));
 
