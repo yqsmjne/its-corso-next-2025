b1dac97d84dd2225522d30099aa98443a55fe700
sofiaspiniella <sofiaspiniella@gmail.com>
2025-02-15 09:23:59 +0100

    fjfj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,13 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
+        <p>ciao raga </p>
         <div className={styles.ctas}>
           <a
             className={styles.primary}
