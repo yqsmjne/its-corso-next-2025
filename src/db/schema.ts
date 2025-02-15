6ab0025f2d7fe83caf0064742bd2959fad02005f
Angiari <02dangiari@gmail.com>
2025-02-15 17:58:56 +0100

    add: route an schema for movie

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -6,3 +6,18 @@ export const usersTable = sqliteTable("users_table", {
   age: int().notNull(),
   email: text().notNull().unique(),
 });
+
+export const moviesTable = sqliteTable("movies_table", {
+  id: int().primaryKey({ autoIncrement: true }),
+  title: text().notNull(),
+  genre: text().notNull(),
+  restriction: text(),
+  language: text().notNull(),
+  director: text().notNull(),
+  description: text().notNull(),
+  year: int().notNull(),
+  image_url: text().notNull(),
+  duration: int().notNull(),
+  trailer_url: text(),
+  rating: int(),
+});
