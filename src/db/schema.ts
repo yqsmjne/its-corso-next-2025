8d3eba3deba807369710b9c18d2812255f06f11d
Marco Bosco <85289871+xFire303@users.noreply.github.com>
2025-02-22 10:42:44 +0100

    add movie, room, schedule, and tag tables with relationships in schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,57 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { int, primaryKey, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const usersTable = sqliteTable("users_table", {
-  id: int().primaryKey({ autoIncrement: true }),
-  name: text().notNull(),
-  age: int().notNull(),
-  email: text().notNull().unique(),
+export const moviesTable = sqliteTable("movies", {
+    id: int("id").primaryKey({ autoIncrement: true }).notNull(),
+    title: text("title").notNull(),
+    genre: text("genre").notNull(),
+    restriction: text("restriction").notNull(),
+    language: text("language").notNull(),
+    director: text("director").notNull(),
+    cast: text("cast").notNull(),
+    description: text("description").notNull(),
+    year: int("year").notNull(),
+    image_url: text("image_url").notNull(),
+    duration: int("duration").notNull(),
+    trailer_url: text("trailer_url").notNull(),
+    rating: int("rating").notNull(),
+    tags_ids: text("tags_ids").notNull(),
 });
+
+export const tagsTable = sqliteTable("tags", {
+    id: int("id").primaryKey({ autoIncrement: true }).notNull(),
+    name: text("name").notNull(),
+})
+
+export const movieTags = sqliteTable("movies_tags", {
+    movie_id: int("id_movie").notNull().references(() => moviesTable.id, { onDelete: "cascade" }),
+    tag_id: int("id_tag").notNull().references(() => tagsTable.id, { onDelete: "cascade" }),
+},
+    (table) => ({
+        pk: primaryKey({columns: [table.movie_id, table.tag_id]}),
+    })
+);
+
+export const roomsTable = sqliteTable("rooms", {
+    id: int("id").primaryKey({ autoIncrement: true }).notNull(),
+    name: text("name").notNull(),
+    available_seats: int("available_seats").notNull(),
+});
+
+export const schedulesTable = sqliteTable("schedules", {
+    id: int("id").primaryKey({ autoIncrement: true }).notNull(),
+    id_movie: int("id_movie").notNull().references(() => moviesTable.id, { onDelete: "cascade" }),
+    id_room: int("id_room").notNull().references(() => roomsTable.id, { onDelete: "cascade" }),
+    date: text("date").notNull(),
+    time: text("time").notNull(),
+    tags_ids: text("tags_ids").notNull(),
+});
+
+export const schedulesTags = sqliteTable("schedules_tags", {
+    schedule_id: int("id_schedule").notNull().references(() => schedulesTable.id, { onDelete: "cascade" }),
+    tag_id: int("id_tag").notNull().references(() => tagsTable.id, { onDelete: "cascade" }),
+},
+    (table) => ({
+        pk: primaryKey({columns: [table.schedule_id, table.tag_id]}),
+    })
+);
+
